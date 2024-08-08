9ec3dcb1b68ad34f97ca107fa1b42b78150781a0
jovanlekic12 <lekicjovan1212@gmail.com>
2024-08-08 11:06:49 +0200

    refactor code and add new methodes

--- main.js
+++ main.js
@@ -16,23 +16,15 @@ class User {
   address;
   friends;
   posts;
-  constructor(
-    firstName,
-    lastName,
-    img,
-    backgroundImg,
-    address,
-    friends,
-    posts
-  ) {
+  constructor(firstName, lastName, img, backgroundImg, address, friends) {
     this.id = crypto.randomUUID();
     this.firstName = firstName;
     this.lastName = lastName;
     this.img = img;
     this.backgroundImg = backgroundImg;
     this.address = address;
-    this.posts = posts;
     this.friends = friends;
+    this.posts = [];
   }
 
   renderFriends() {
@@ -48,6 +40,10 @@ class User {
       </li>`;
       friendsList.insertAdjacentHTML("afterbegin", html);
     });
+  }
+
+  addPost(post) {
+    this.posts.push(post);
   }
 
   renderPosts() {
@@ -77,8 +73,27 @@ class User {
       } comments</p>
         </div>
       </div>
+      <div class="post__buttons">
+        <button class="post__btn btn__like"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+          <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+          </svg><span>Like</span>
+        </button>
+        <button class="post__btn btn__comment"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+          <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
+          </svg><span>Comment</span>
+        </button>
+      </div>
+      <div class="add__comment__div">
+      <img class="post__user__photo" src="${this.img}" alt="404" />
+      <form class="add__comment__form">
+      <input type="text" class="add__comment__input" placeholder="Write a comment" required/>
+      <button class="add__comment__btn">Add comment</button>
+      </form>
+      </div>
       </li>`;
       postsList.insertAdjacentHTML("afterbegin", html);
+      const likeParagraph = document.querySelector(`.likes__p__${post.id}`);
+      post.renderLikes(likeParagraph);
     });
   }
 }
@@ -90,11 +105,19 @@ class Post {
   likes;
   comments;
   constructor(postText, postDate) {
-    id = crypto.randomUUID();
+    this.id = crypto.randomUUID();
     this.postText = postText;
     this.postDate = postDate;
     this.likes = [];
     this.comments = [];
+  }
+
+  addComment(comment) {
+    this.comments.push(comment);
+  }
+
+  addLike(like) {
+    this.likes.push(like);
   }
 
   renderLikes(likeParagraph) {
@@ -145,4 +168,28 @@ const user1 = new User(
 );
 
 user1.renderFriends();
+user.posts.forEach((post) => {
+  const newPost = new Post(
+    post.postText,
+    post.postDate,
+    post.likes,
+    post.comments
+  );
+  post.comments.forEach((comment) => {
+    const newComment = new Comment(
+      comment.firstName,
+      comment.lastName,
+      comment.commentText,
+      comment.img
+    );
+    newPost.addComment(newComment);
+  });
+  post.likes.forEach((like) => {
+    const newLike = new Like(like.firstName, like.lastName);
+    newPost.addLike(newLike);
+  });
+
+  user1.addPost(newPost);
+});
 user1.renderPosts();
+console.log(user1);
